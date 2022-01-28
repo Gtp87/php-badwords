ba8952c4f0a592db18dba936ffa8cfed997eef6c
Gtp87 <fabio.giannitrapani@gmail.com>
2022-01-28 14:05:29 +0100

    punto 1

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <!-- Creare una variabile con un paragrafo di testo a vostra scelta.
-Stampare a schermo il paragrafo e la sua lunghezza.
-Una parola da censurare viene passata dall’utente tramite parametro GET.
-Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+1-Stampare a schermo il paragrafo e la sua lunghezza.
+2-Una parola da censurare viene passata dall’utente tramite parametro GET.
+3-Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+
+
+<?php 
+// scelgo paragrafo
+$text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente autem natus corporis tempora eveniet, ab nesciunt quod, temporibus a sequi dolor eius dolore commodi eum praesentium, repudiandae fuga? Aut, eos.";
+
+// controllo la lunghezza
+$textLen = strlen($text);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+</head>
+<body>
+    <h4><?php echo $text ?></h4>
+    <h5>Lunghezza: <?php echo $textLen?></h5>
+
+</body>
+</html>
